0dc9e621019cb736d9bd0e4e443aefe246a23d4a
alaedinne <alaedinne.marzougui@esprit.tn>
2020-08-14 14:28:46 +0200

    first commit project pinterest dev

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+
+    /**
+     * @Route("/" , name="app_home" , methods={"GET"})
+     */
+    public function index(PinRepository $repo): Response
+    {
+
+         return  $this->render('pins/index.html.twig' , ['pins' => $repo->findAll()]) ;
+
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}")
+     */
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', ['pin' => $pin]);
+    }
+
+    /**
+     * @Route("/pins/create", name="app_pins_create" ,methods={"GET" , "POST"})
+     */
+    public function  create( Request $request , EntityManagerInterface $em ): Response
+    {
+
+          $pin = new Pin();
+
+       $form = $this->createFormBuilder($pin)
+        ->add('title' , null , [
+            'attr' => ['autofocus' => true]
+        ])
+        ->add('description' )
+        ->getForm();
+        ;
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+               $em->persist($pin);
+               $em->flush();
+
+               return $this->redirectToRoute("app_pins_show" , ['id' => $pin->getId()]);
+
+
+
+        }
+
+       return  $this->render('pins/create.html.twig' , [
+           'form' => $form->createView()
+       ]);
+    }
+
+}
